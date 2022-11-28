296ea2dfd8bf311ec3e49704f253c46ad18b768f
Giosuè Iaccarino <giosue@iacca.ml>
2022-11-28 10:50:12 +0100

    🩹 Corrected background color

--- index.php
+++ index.php
@@ -15,9 +15,9 @@ require_once "includes/components/templates/template.php";
 	<?php include "includes/components/structure/head.php";?>
 </head>
 
-<body class="select-none h-screen">
+<body class="select-none h-screen bg-base-200">
 
-	<main id="swup" class="">
+	<main id="swup" class="bg-base-200">
 
 		<?php include "includes/components/structure/navigations/main/top.php";?>
 
@@ -25,21 +25,27 @@ require_once "includes/components/templates/template.php";
 
 			<!---Quick action button-->
 			<div class="container max-w-3xl mx-auto px-6">
-		
+
 			</div>
 			<!---End action button-->
 
 			<div class="container max-w-3xl mx-auto pb-24">
 
 				<div class="px-4 py-4">
-					<h1 class="text-4xl font-bold text-base-content">
+					<h1 class="text-4xl font-bold text-base-content mb-4">
 						<?php if (USER_IS_LOGGED): ?>
-						<span class="text-accent">Ciao</span> <?=htmlspecialchars(USER['name'])?>
+						<!-- <span class="text-accent">Ciao</span> <?=htmlspecialchars(USER['name'])?> -->
 						<?php else:?>
 						<span class="text-accent">Accedi</span> a SegatoBrust App
 						<?php endif;?>
 					</h1>
-					
+
+					<?php if (USER_IS_LOGGED): ?>
+					<h4 class="text-lg font-semibold">
+						Orario di oggi
+					</h4>
+					<?php template_HTML("calendars/class_schedule")?>
+					<?php endif;?>
 				</div>
 
 			</div>
