4ffe1bb7ee37e08fde6fbc4b40cceb0118909266
Giosuè Iaccarino <giosue@iacca.ml>
2022-12-17 18:01:43 +0100

    🐛 Dashboard messages sorted by date

--- includes/utils/database/dashboard_messages.php
+++ includes/utils/database/dashboard_messages.php
@@ -6,7 +6,7 @@ require_once "includes/utils/database/database_connection.php";
 function get_dashboard_messages() {
 	$pdo = pdo_connection();
 
-	$stmt = $pdo->prepare("SELECT * FROM dashboard_messages");
+	$stmt = $pdo->prepare("SELECT * FROM dashboard_messages ORDER BY creation_date DESC");
 	$stmt->execute([]);
 	return $stmt->fetchAll(\PDO::FETCH_ASSOC);
 }
