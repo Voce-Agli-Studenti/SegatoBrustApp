a87203747382539643840d4a40f66e5eeb35fbd1
Giosuè Iaccarino <giosue@iacca.ml>
2022-12-23 11:31:30 +0100

    ✨ Added school news on the home page

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ define("NAVIGATION_PAGE", "home");
 require_once "includes/utils/session.php";
 require_once "includes/components/templates/template.php";
 
+$com = file_get_contents("https://scuola.gioiacca9.tk/api/comunicati/famiglia/?limit=2");
+$com = json_decode($com, true);
+
 ?>
 
 <!DOCTYPE html>
@@ -34,17 +37,36 @@ require_once "includes/components/templates/template.php";
 				<div class="px-4 py-4">
 					<?php template_HTML("sections/dashboard_messages");?>
 
-					<h1 class="text-4xl font-bold text-base-content mb-4">
+					<h1 class="text-4xl font-bold text-base-content my-3">
 						<?php if (USER_IS_LOGGED): ?>
 						<?php else: ?>
-						<span class="text-accent">Accedi</span> a SegatoBrust App
+						<span class="text-accent"><a href="/login/">Accedi</a></span> a SegatoBrust App
 						<?php endif;?>
 					</h1>
 
 					<?php if (USER_IS_LOGGED): ?>
 					<?php template_HTML("calendars/class_schedule");?>
-
 					<?php endif;?>
+
+					<div class="">
+						<h4 class="text-lg font-semibold">
+							Ultimi comunicati
+						</h4>
+						<div class="my-3">
+							<?php for ($i = 0; $i < count($com) && $i < 50; $i++): ?>
+							<a href="https://data.iacca.ml/articleextractor/?disable_proxy&id=<?=$com[$i]['comunicato_id'];?>"
+								target="_blank">
+								<div class="card rounded mb-1 bg-base-100">
+									<div class="card-body">
+										<span class="text-lg font-bold truncate-2">
+											<?=htmlspecialchars($com[$i]['title']);?>
+										</span>
+									</div>
+								</div>
+							</a>
+							<?php endfor;?>
+						</div>
+					</div>
 
 				</div>
 
